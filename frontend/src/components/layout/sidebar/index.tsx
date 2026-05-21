e4ba2516158ba885a13de792f45f3405bf622543
myon <ni.tai011106@gmail.com>
2026-05-21 15:43:20 +0900

    通知画面をヘッダーに移動

--- frontend/src/components/layout/sidebar/index.tsx
+++ frontend/src/components/layout/sidebar/index.tsx
@@ -13,8 +13,7 @@ import type { MenuItem } from "./types";
 
 const baseMenuList: MenuItem[] = [
     { name: "投稿一覧", path: "/posts", icon: Home },
-    { name: "授業評価", path: "/courses", icon: BookOpen },
-    { name: "通知", path: "/notifications", icon: Bell },
+    //{ name: "通知", path: "/notifications", icon: Bell },
     { name: "TGカレンダー", path: "/calendar", icon: Calendar },
     { name: "外部サイト", path: "/external", icon: ExternalLink },
     { name: "お問い合わせ", path: "/contact", icon: MessageSquare },
