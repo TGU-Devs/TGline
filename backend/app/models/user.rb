fe9128425b843b927c6066f536449a07edf331bb
Dev-koya-js <koya.god58@gmail.com>
2026-07-17 23:10:33 +0900

    Active Storageにavaterを宣言

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -3,6 +3,7 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_one_attached :avatar
   has_many :posts, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :comments, dependent: :destroy
